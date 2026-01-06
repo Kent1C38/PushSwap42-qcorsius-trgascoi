4ef5b9b9970bb362e9f3187b284dc10043e63ad7
Kent1C8 <corsiusquentin@gmail.com>
2026-01-06 10:40:15 +0100

    Added utils for stacks

--- stack_utils.c
+++ stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: qcorsius <qcorsius@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 10:27:19 by qcorsius          #+#    #+#             */
-/*   Updated: 2026/01/06 10:04:29 by qcorsius         ###   ########lyon.fr   */
+/*   Updated: 2026/01/06 10:37:32 by qcorsius         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,26 @@ t_stack	*new_stack(int value)
 	return (new);
 }
 
-
-void	push_stack(int value, t_stack *stack)
+void	push_stack(int value, t_stack **stack)
 {
 	t_stack	*modified;
 
 	modified = new_stack(value);
-	modified->next = stack;
-	stack->previous = modified;
-	stack = modified;
+	modified->next = *stack;
+	(*stack)->previous = modified;
+	*stack = modified;
 }
 
-int	pop_stack(t_stack *stack)
+int	pop_stack(t_stack **stack)
 {
-	
-}
+	int		value;
+	t_stack	*new_stack;
 
+	value = (*stack)->value;
+	new_stack = (*stack)->next;
+	(*stack)->next->previous = NULL;
+	(*stack)->next = NULL;
+	free(*stack);
+	*stack = new_stack;
+	return (value);
+}
