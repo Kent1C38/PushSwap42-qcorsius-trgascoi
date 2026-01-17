15a6a04d892c1989e09858f9d1a25f239d87839d
Kent1C8 <corsiusquentin@gmail.com>
2026-01-17 12:54:32 +0100

    Optimized Radix sort

--- radix_sort.c
+++ radix_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   radix_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: trgascoi <trgascoi@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: qcorsius <qcorsius@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 00:00:00 by trgascoi          #+#    #+#             */
-/*   Updated: 2026/01/08 14:56:53 by trgascoi         ###   ########.fr       */
+/*   Updated: 2026/01/17 12:42:13 by qcorsius         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,16 @@ static int	get_max_bits(int size)
 	return (bits);
 }
 
-static void	sort_by_bit(t_identified_stack *a, t_identified_stack *b,
-	t_sort_ctx *ctx, int bit)
+static void	radix_push_from_a(t_identified_stack *a, t_identified_stack *b,
+		t_sort_ctx *ctx, int bit)
 {
 	int	i;
+	int	size_a;
 	int	rank;
 
 	i = 0;
-	while (i < ctx->size)
+	size_a = get_stack_size(a);
+	while (i < size_a)
 	{
 		rank = get_rank(a->content->value, ctx->sorted, ctx->size);
 		if (!((rank >> bit) & 1))
@@ -47,10 +49,26 @@ static void	sort_by_bit(t_identified_stack *a, t_identified_stack *b,
 	}
 }
 
-static void	push_all_back(t_identified_stack *a, t_identified_stack *b)
+static void	radix_push_from_b(t_identified_stack *a, t_identified_stack *b,
+	t_sort_ctx *ctx, int bit)
 {
-	while (b->content != NULL)
-		push(b, a);
+	int	i;
+	int	size_b;
+	int	rank;
+	int	max_bits;
+
+	i = 0;
+	size_b = get_stack_size(b);
+	max_bits = get_max_bits(ctx->size);
+	while (i < size_b)
+	{
+		rank = get_rank(b->content->value, ctx->sorted, ctx->size);
+		if ((rank >> (bit + 1)) & 1 || bit == max_bits - 1)
+			push(b, a);
+		else
+			rotate(b);
+		i++;
+	}
 }
 
 void	radix_sort(t_identified_stack *a, t_identified_stack *b)
@@ -66,12 +84,11 @@ void	radix_sort(t_identified_stack *a, t_identified_stack *b)
 	if (!ctx.sorted)
 		return ;
 	max_bits = get_max_bits(ctx.size);
-	bit = 0;
-	while (bit < max_bits)
+	bit = -1;
+	while (++bit < max_bits)
 	{
-		sort_by_bit(a, b, &ctx, bit);
-		push_all_back(a, b);
-		bit++;
+		radix_push_from_a(a, b, &ctx, bit);
+		radix_push_from_b(a, b, &ctx, bit);
 	}
 	free(ctx.sorted);
 }
